a2abe612dc68ab940f48e3e11db5d99de866462f
Vinaya541 <vinayavinnu541@gmail.com>
2016-09-05 20:29:58 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Alpha
+{
+ public static void main(String args[])
+ {
+  char n;
+  DataInputStream ds=new DataInputStream(System.in);
+  n=(char)ds.read(); 
+  if(Character.isLetter(n))
+      System.out.println("is an alphabet");
+  else
+      System.out.println("is not an alphabet");
+ }
+}
