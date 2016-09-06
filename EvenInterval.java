f564eb7870dbca2418883a4fb1c6b2f330367787
Vinaya541 <vinayavinnu541@gmail.com>
2016-09-06 20:19:08 +0530

    Create EvenInterval.java

---
+++ EvenInterval.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class EvenInterval
+{
+ public static void main (String[] args)throws IOException
+ {
+	int  a, b;
+  DataInputStream ds=new DataInputStream(System.in);
+  a=Integer.parseInt(ds.readLine());
+  b=Integer.parseInt(ds.readLine());
+  while(a<=b)
+  {
+   if(a%2==0)
+      System.out.println(+a);
+   a++;
+  }
+ }
+}
