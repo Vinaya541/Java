8f9480693ea2b6772f9b06b1898646d6168f7b8b
Vinaya541 <vinayavinnu541@gmail.com>
2016-09-06 18:31:04 +0530

    Update SumOfNatural.java

--- SumOfNatural.java
+++ SumOfNatural.java
@@ -3,12 +3,13 @@ class SumOfNatural
 {
  public static void main(String args[])throws IOException
  {
-  int n, s=0;
+  int n, s=0, i=1;
   DataInputStream ds=new DataInputStream(System.in);
   n=Integer.parseInt(ds.readLine());
-  for(int i=1; i<=n; i++)
+  while(i<=n)
   {
       s=s+i;
+      i++;
   }
   System.out.println(s);
  }
